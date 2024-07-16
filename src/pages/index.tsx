d001fc5cc59a9ec611ba9b7831f1c41561fbd31f
srujana85 <srujanaaa99@gmail.com>
2024-07-16 12:04:56 +0300

    switch to setup using TypeScript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1>Initial Portfolio setup</h1>
+      <h1>Intial Setup of TechStack</h1>
     </main>
   );
 }
