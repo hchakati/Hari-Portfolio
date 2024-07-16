777e6757ed6af7bc0771bac52e4943efcd5a812f
srujana85 <srujanaaa99@gmail.com>
2024-07-16 18:29:03 +0300

    base path set to /srujana-portfolio/

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export"
+	reactStrictMode: true,
+	output: "export",
+	basePath: "/srujana-portfolio/",
 };
 
 export default nextConfig;
